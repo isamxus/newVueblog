7350cf7c56b1f387f197529f73f4683c30349cd0
isamxus <834775778@qq.com>
2020-03-15 07:45:12 +0800

    boke

--- blogAdmin/views.py
+++ blogAdmin/views.py
@@ -6,6 +6,21 @@ from .models import ParamsSettings,ParamsContent
 import json
 import simplejson
 from django.core import serializers
+
+#请求成功返回
+SuccessMsg = {
+	'status':True,
+	'Msg': '成功！！！'
+}
+
+
+#请求失败返回
+FailedMsg = {
+	'status':False, 
+	'PostContent':'failed', 
+	'Msg': '服务器发生错误，请联系管理员！！！'
+}
+
 # Create your views here.
 def RequestHandler(request):
 	return json.loads(json.dumps(request.POST))
@@ -20,10 +35,9 @@ def createParamsHandler(request):
 			paramsCode=requestData['paramsCode']
 		)
 		newParam.save()
-		return JsonResponse({'status':True, 'PostContent':'success'})
+		return JsonResponse(dict({'PostContent':'success'}, **SuccessMsg))
 	except:
-		print
-		return JsonResponse({'status':False, 'PostContent':'failed'})
+		return JsonResponse(FailedMsg)
 
 #获取参数列表
 @csrf_exempt
@@ -37,6 +51,6 @@ def getParamsListHandler(request):
 			dict['id'] = obj['pk']
 			return dict
 		PostContent = list(map(fn, json.loads(serializers.serialize('json',PostContent))))
-		return JsonResponse({'status':True, 'PostContent':PostContent})
+		return JsonResponse(dict({'PostContent':PostContent}, **SuccessMsg))
 	except:
-		return JsonResponse({'status':False, 'PostContent':'failed'})
+		return JsonResponse(FailedMsg)
